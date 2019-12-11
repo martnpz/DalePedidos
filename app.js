7302a663cf586b502fc3e31edf2337a2b6977223
MartinMAPerez <martinmatiasperez2015@gmail.com>
2019-12-10 23:21:48 -0300

    Aplicacion mobil pre lease

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var boton = document.getElementById("menuOpen")
 var menuExpanded = document.getElementById("hamburgerMenu")
+var backMenu = document.getElementById("backMenu")
 
-boton.addEventListener('click', showMenu)
+boton.addEventListener('click', showMenu);
+backMenu.addEventListener('click', showMenu);
 
 function showMenu(event){
     event.preventDefault();
     menuExpanded.classList.toggle("menuExpanded");
+}
+
+function showHeaderBar(event){
+    console.log("funciona");
 }
